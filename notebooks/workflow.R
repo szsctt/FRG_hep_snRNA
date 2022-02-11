72a2e0375274603902161fae7d4065150f4e27f5
Suzanne Scott <suzanne.scott@csiro.au>
2022-02-11 04:15:54 +0000

    better cell labels for aizarani dataset, and investigated zonation in aizarani dataset"

--- notebooks/workflow.R
+++ notebooks/workflow.R
@@ -184,7 +184,7 @@ import_aizarani <- function() {
   aizarani<- RunUMAP(aizarani, dims = 1:30, reduction = "pca", return.model = TRUE)
   
   paper.cluster.names <- tibble(
-    name = c("NK, NKT, T cells", "Kupffer cells", "NK, NKT, T cells", "EPCAM+ cells and cholangiocytes", "NK, NKT, T cells", "Kupffer cells", "EPCAM+ cells and cholangiocytes", "B cells", "Liver sinusoidal endothelial cells", "Macrovascular endothlial cells", "Hepatocytes", "NK, NKT, T cells", "Liver sinusoidal endothelial cells", "Hepatocytes", "Other endothelial cells", "Other", "Hepatocytes", "NK, NKT, T cells", "Other", "Liver sinusoidal endothelial cells", "Stellate cells and myofibroblasts", "B cells", "Kupffer cells", "EPCAM+ cells and cholangiocytes", "Kupffer cells", "Other endothelial cells", "Other", "NK, NKT, T cells", "Macrovascular endothlial cells", "Hepatocytes", "Kupffer cells", "NK, NKT, T cells", "Stellate cells and myofibroblasts", "B cells", "Other endothelial cells", "Other", "Other", "B cells", "Other"),
+    name = c("NK, NKT, T cells", "Kupffer cells", "NK, NKT, T cells", "EPCAM+ cells and cholangiocytes", "NK, NKT, T cells", "Kupffer cells", "EPCAM+ cells and cholangiocytes", "B cells", "Liver sinusoidal endothelial cells", "Macrovascular endothelial cells", "Hepatocytes", "NK, NKT, T cells", "Liver sinusoidal endothelial cells", "Hepatocytes", "Other endothelial cells", "Other", "Hepatocytes", "NK, NKT, T cells", "Other", "Liver sinusoidal endothelial cells", "Stellate cells and myofibroblasts", "B cells", "Kupffer cells", "EPCAM+ cells and cholangiocytes", "Kupffer cells", "Other endothelial cells", "Other", "NK, NKT, T cells", "Macrovascular endothelial cells", "Hepatocytes", "Kupffer cells", "NK, NKT, T cells", "Stellate cells and myofibroblasts", "B cells", "Other endothelial cells", "Other", "Other", "B cells", "Other"),
     cluster = seq(length(name))
   )
   
@@ -198,6 +198,17 @@ import_aizarani <- function() {
     left_join(paper.cluster.names, by="cluster")
   
   aizarani[['paper.cluster.names']] <- aizarani_cellnames$name
+  
+  aizarani$cell.types <- case_when(
+    aizarani$seurat_clusters %in% c(0, 6, 14) ~ "NK, NKT, T cells",
+    aizarani$seurat_clusters == 1 ~ "Liver sinusoidal endothelial cells",
+    aizarani$seurat_clusters %in% c(2, 13) ~ "EPCAM+ cells and cholangiocytes",
+    aizarani$seurat_clusters %in% c(3, 5, 7, 9, 16) ~ "Hepatocytes",
+    aizarani$seurat_clusters %in% c(4, 10) ~ "Kupffer cells",
+    aizarani$seurat_clusters == 8 ~ "Macrovascular endothelial cells",
+    aizarani$seurat_clusters %in% c(11, 12) ~ "B cells",
+    aizarani$seurat_clusters == 15 ~ "Stellate cells and myofibroblasts"
+  )
   
   return(aizarani)
   
@@ -221,7 +232,9 @@ integrated_with_aizarani <- function() {
                        reference=aizarani,
                        query=query, 
                        refdata = list(paper.cluster.names="paper.cluster.names",
-                                      paper.cluster="paper.clusters"), 
+                                      paper.cluster="paper.clusters",
+                                      cell.type="cell.types",
+                                      seurat.cluster="seurat_clusters"), 
                        reference.reduction="pca",
                        reduction.model = "umap")
     p <- DimPlot(query, reduction="ref.umap", group.by="predicted.paper.cluster.names") + ggtitle(glue::glue("{query_name}, k.filter = {k.filter}"))
