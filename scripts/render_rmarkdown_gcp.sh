de210f1eb2f8362ac0dfe799995cbc395d7d1055
Suzanne Scott <suzannesctt@gmail.com>
2022-10-30 07:16:27 +0000

    update

--- scripts/render_rmarkdown_gcp.sh
+++ scripts/render_rmarkdown_gcp.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#SBATCH --job-name=liver_scRNA_rmarkdown
-#SBATCH --nodes=1 --ntasks=1
-#SBATCH --time=12:00:00
-#SBATCH --mem=50gb
-#SBATCH --output=../log/rmarkdown_%A_%a.out
-#SBATCH --error=../log/rmarkdown_%A_%a.out
-#SBATCH --array=1-5
-
 set -euo pipefail
 
 if [ ! -e scrna_rstudio.sif ]; then
@@ -18,7 +10,7 @@ fi
 cd ../notebooks
 mkdir -p rendered
 
-RSCRIPT="singularity exec ../scripts/scrna_rstudio.sif ../scripts/render.R rendered"
+RSCRIPT="singularity exec -B$(realpath ..):$(realpath ..) ../scripts/scrna_rstudio.sif ../scripts/render.R rendered"
 
 
 eval "${RSCRIPT} vector.Rmd"
@@ -29,9 +21,12 @@ eval "${RSCRIPT} integration.Rmd"
 
 eval "${RSCRIPT} figure1.Rmd"
 
+eval "${RSCRIPT} extra_figures_for_reviewers.Rmd"
+
 eval "${RSCRIPT} aizarani_integration_hepatocytes.Rmd"
 
 eval "${RSCRIPT} integrated_pseudotime.Rmd"
 
 eval "${RSCRIPT} aizarani_integration_tests.Rmd"
+
 
